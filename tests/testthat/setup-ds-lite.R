002967ce46feb24685444c1763fca0cfb09eaae6
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-03-10 16:49:59 +0100

    first commit

---
+++ tests/testthat/setup-ds-lite.R
@@ -0,0 +1,24 @@
+library(DSLite)
+library(devtools)
+
+setupDSLite <- function() {
+  options(datashield.env = environment())
+  dslite.server <- DSLite::newDSLiteServer()
+  load_all("~/Library/Mobile Documents/com~apple~CloudDocs/work/repos/dsExample")
+  dslite.server$config(defaultDSConfiguration(include=c("dsBase", "dsExample")))
+  dslite.server$aggregateMethod("funLevelsDS", "funLevelsDS")
+  dslite.server$aggregateMethod("listDisclosureSettingsDS", "listDisclosureSettingsDS")
+
+  builder <- DSI::newDSLoginBuilder()
+
+  builder$append(
+    server = "server_1",
+    url = "dslite.server",
+    driver = "DSLiteDriver"
+  )
+
+  logindata <- builder$build()
+  conns <- DSI::datashield.login(logins = logindata, assign = FALSE)
+  return(conns)
+}
+
